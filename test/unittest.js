94a85d2d24eb3ccbfc0e3c4fe928e8cbe6da3e84
Andrey Galkin <andrey@futoin.org>
2019-11-06 23:16:32 +0200

    NEW: initial folder structure

---
+++ test/unittest.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const expect = require( 'chai' ).expect;
+
+const $as_test = require( 'futoin-asyncsteps/testcase' );
+const AdvancedCCM = require( 'futoin-invoker/AdvancedCCM' );
+const Executor = require( 'futoin-executor/Executor' );
+const SpecTools = require( 'futoin-invoker/SpecTools' );
+
+SpecTools.on( 'error', ( ...args ) => console.log( args ) );
+
+describe( '', function() {
+} );
+
+
